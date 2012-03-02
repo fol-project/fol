b657622c27c0200453fd382ee275bc5ac81a43a6
Óscar Otero <oom@oscarotero.com>
2012-03-02 20:06:13 +0100

    movido o arquivo functions.php

--- apps/Web/Controllers/Main.php
+++ apps/Web/Controllers/Main.php
@@ -7,6 +7,8 @@ use Fol\Http\Response;
 class Main extends Controller {
 
 	public function index () {
+		//include ($this->App->path.'helpers/functions.php');
+
 		$contido = $this->Views->render('html.php', array(
 			'titulo' => 'Ola mundo',
 			'estilos' => 'css/csans.css'
