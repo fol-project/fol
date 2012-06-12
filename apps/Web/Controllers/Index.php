4e222d23ea140a930e3e1b5a709e9e264e9d0cbc
Óscar Otero <oom@oscarotero.com>
2012-06-12 22:08:59 +0200

    deleted some files

--- apps/Web/Controllers/Index.php
+++ apps/Web/Controllers/Index.php
@@ -7,55 +7,10 @@ use Fol\Templates;
 class Index {
 
 	public function __construct () {
-		$this->Templates = new Templates($this->App->getPath().'templates');
-		$this->Templates->App = $this->App;
-		$this->Mustache = new \Mustache();
 	}
 
 	public function index () {
 		return new Response('Ola mundo');
-	}
-
-	public function layout ($name) {
-		if (!($layout_dir = $this->getLayoutPath($name))) {
-			return new Response('This layout does not exists', 404);
-		}
-
-		$options = array_replace_recursive(include($layout_dir.'options.php'), $this->Request->Get->get());
-
-		$data = array(
-			'name' => $name,
-			'options' => $options
-		);
-
-		$this->Templates->register('layout', $layout_dir.'html.php');
-
-		return new Response($this->Templates->render('layout.php', $data));
-	}
-
-	public function styles ($name) {
-		if (!($layout_dir = $this->getLayoutPath($name))) {
-			return new Response('This layout does not exists', 404);
-		}
-
-		$data = array_replace_recursive(include($layout_dir.'options.php'), $this->Request->Get->get());
-
-		$styles = $this->Mustache->render(file_get_contents($layout_dir.'styles.mustache'), $data);
-
-		$response = new Response($styles);
-		$response->setContentType('text/css');
-
-		return $response;
-	}
-
-	private function getLayoutPath ($name) {
-		$path = $this->App->getPath().'templates/layouts/'.strtolower($name);
-
-		if (!is_dir($path)) {
-			return false;
-		}
-
-		return $path.'/';
 	}
 }
 ?>
--- apps/Web/templates/fieldset.php
+++
@@ -1,6 +0,0 @@
-<fieldset>
-	<legend><?php echo $legend; ?></legend>
-	<?php foreach ($inputs as $label => $value): ?>
-	<label><?php echo $label ?><input type="text" name="<?php echo $key.'['.$label.']'; ?>" value="<?php echo $value; ?>"></label><br>
-	<?php endforeach; ?>
-</fieldset>
