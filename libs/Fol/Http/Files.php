19fe01b979fff19640d3610ee19c1ee4c4992fd1
Óscar Otero <oom@oscarotero.com>
2012-09-10 15:03:09 +0200

    Engadidas funcións de erros en ->Files

--- libs/Fol/Http/Files.php
+++ libs/Fol/Http/Files.php
@@ -78,5 +78,59 @@ class Files extends Input {
 
 		return $results;
 	}
+
+
+	/**
+	 * Check if an uploaded file has any error
+	 * 
+	 * @param string $name The name of the uploaded file
+	 * 
+	 * @return boolean True if has an error, false if not
+	 */
+	public function hasError ($name) {
+		$file = $this->get($name);
+
+		if (isset($file['error']) && $file['error'] > 0) {
+			return true;
+		}
+
+		return false;
+	}
+
+
+	/**
+	 * Returns the error code
+	 * 
+	 * @param string $name The name of the uploaded file
+	 * 
+	 * @return int The error code or null if the file doesn't exist
+	 */
+	public function getErrorCode ($name) {
+		$file = $this->get($name);
+
+		if (isset($file['error'])) {
+			return $file['error'];
+		}
+
+		return null;
+	}
+
+
+	/**
+	 * Returns the error message
+	 * 
+	 * @param string $name The name of the uploaded file
+	 * 
+	 * @return string The error message or null if the file doesn't exist
+	 */
+	public function getErrorMessage ($name) {
+		$code = $this->getErrorCode($name);
+
+		if (!isset($code) || !isset(self::$errors[$code])) {
+			return null;
+		}
+
+		return self::$errors[$code];
+	}
 }
 ?>
