959d756a7eaba516f71d13e4562876b749042b83
Óscar Otero <oom@oscarotero.com>
2013-03-27 01:26:37 +0100

    engadida a magic function __set_state en cookies

--- libs/Fol/Http/Cookies.php
+++ libs/Fol/Http/Cookies.php
@@ -22,6 +22,16 @@ class Cookies {
 
 
 	/**
+	 * Function executed only to restore a previous saved state
+	 * 
+	 * @param array $items Items to restore
+	 */
+	public function setState ($items) {
+		$this->items = $items;
+	}
+
+
+	/**
 	 * Magic function to converts all cookies to a string
 	 */
 	public function __toString () {
