83c70c4ba8f0da5becfed73d1fb3d421d5ce9c46
oscarotero <oom@oscarotero.com>
2013-10-18 14:35:14 +0200

    Eliminado o codigo para rexistrar e executar as apps
    Xa o inserta automaticamente o app-installer

--- index.php
+++ index.php
@@ -5,11 +5,8 @@ use Fol\Errors;
 include('bootstrap.php');
 
 //Register the apps here
-Loader::registerNamespace('Apps\\Web', BASE_PATH.'/web');
 
 //Show errors
 Errors::displayErrors();
 
 //Handle the request and send the response
-$app = new Apps\Web\App;
-$app()->send();
