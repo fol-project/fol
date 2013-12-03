c44169f5b3a518be8947d6e6ee3abe58bebe34f2
Óscar Otero <oom@oscarotero.com>
2013-12-03 21:48:12 +0100

    Engadida a posibilidade de cambiar BASE_ABSOLUTE_URL accedendo por cli

--- bootstrap.php
+++ bootstrap.php
@@ -6,8 +6,14 @@ define('ACCESS_INTERFACE', (php_sapi_name() === 'cli') ? 'cli' : 'http');
 define('BASE_PATH', str_replace('\\', '/', __DIR__));
 
 if (ACCESS_INTERFACE === 'cli') {
+	if (isset($argv[1]) && (strpos($argv[1], '//') !== false) && (($components = parse_url($argv[1])) !== false)) {
+		define('BASE_ABSOLUTE_URL', (isset($components['scheme']) ? $components['scheme'] : 'http').'://'.$components['host']);
+		unset($components);
+	} else {
+		define('BASE_ABSOLUTE_URL', 'http://localhost');
+	}
+
 	define('BASE_URL', '');
-	define('BASE_ABSOLUTE_URL', 'http://localhost');
 } else {
 	define('BASE_URL', preg_replace('|/+|', '/', strtolower(preg_replace('|^'.str_replace('\\', '/', realpath($_SERVER['DOCUMENT_ROOT'])).'|i', '', BASE_PATH))));
 	define('BASE_ABSOLUTE_URL', ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') ? 'https' : 'http').'://'.$_SERVER['HTTP_HOST']);
