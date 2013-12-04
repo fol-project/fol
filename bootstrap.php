e7fb4fefa9df7caabde458b54c669f4b1bc5212c
oscarotero <oom@oscarotero.com>
2013-12-04 11:14:10 +0100

    Probas para definir BASE_URL dende cli

--- bootstrap.php
+++ bootstrap.php
@@ -6,18 +6,21 @@ define('ACCESS_INTERFACE', (php_sapi_name() === 'cli') ? 'cli' : 'http');
 define('BASE_PATH', str_replace('\\', '/', __DIR__));
 
 if (ACCESS_INTERFACE === 'cli') {
+	$documentRoot = dirname(__DIR__);
+
 	if (isset($argv[1]) && (strpos($argv[1], '://') !== false) && (($components = parse_url($argv[1])) !== false)) {
 		define('BASE_ABSOLUTE_URL', $components['scheme'].'://'.$components['host']);
 		unset($components);
 	} else {
 		define('BASE_ABSOLUTE_URL', 'http://localhost');
 	}
-
-	define('BASE_URL', '');
 } else {
-	define('BASE_URL', preg_replace('|/+|', '/', strtolower(preg_replace('|^'.str_replace('\\', '/', realpath($_SERVER['DOCUMENT_ROOT'])).'|i', '', BASE_PATH))));
+	$documentRoot = realpath($_SERVER['DOCUMENT_ROOT']);
 	define('BASE_ABSOLUTE_URL', ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') ? 'https' : 'http').'://'.$_SERVER['HTTP_HOST']);
 }
+
+define('BASE_URL', preg_replace('|/+|', '/', strtolower(preg_replace('|^'.str_replace('\\', '/', $documentRoot).'|i', '', BASE_PATH))));
+unset($documentRoot);
 
 include(BASE_PATH.'/libs/Fol/Loader.php');
 
