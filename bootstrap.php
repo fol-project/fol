4839cf6b7191e285b5c28b860b4730e3bf5107fd
Óscar Otero <oom@oscarotero.com>
2013-12-07 18:30:33 +0100

    Proposta 3 #2

--- bootstrap.php
+++ bootstrap.php
@@ -6,21 +6,24 @@ define('ACCESS_INTERFACE', (php_sapi_name() === 'cli') ? 'cli' : 'http');
 define('BASE_PATH', str_replace('\\', '/', __DIR__));
 
 if (ACCESS_INTERFACE === 'cli') {
-	$documentRoot = dirname(__DIR__);
+	$components = [
+		'scheme' => 'http',
+		'host' => 'localhost',
+		'path' => ''
+	];
 
-	if (isset($argv[1]) && (strpos($argv[1], '://') !== false) && (($components = parse_url($argv[1])) !== false)) {
-		define('BASE_ABSOLUTE_URL', $components['scheme'].'://'.$components['host']);
-		unset($components);
-	} else {
-		define('BASE_ABSOLUTE_URL', 'http://localhost');
+	if (!empty($argv[1]) && preg_match('|^(\{([^\}]+)\})|', $argv[1], $match)) {
+		$components = parse_url($match[2]) + $components;
 	}
+	
+	define('BASE_ABSOLUTE_URL', $components['scheme'].'://'.$components['host']);
+	define('BASE_URL', $components['path']);
+
+	unset($components, $match);
 } else {
-	$documentRoot = realpath($_SERVER['DOCUMENT_ROOT']);
+	define('BASE_URL', preg_replace('|/+|', '/', strtolower(preg_replace('|^'.str_replace('\\', '/', realpath($_SERVER['DOCUMENT_ROOT'])).'|i', '', BASE_PATH))));
 	define('BASE_ABSOLUTE_URL', ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') ? 'https' : 'http').'://'.$_SERVER['HTTP_HOST']);
 }
-
-define('BASE_URL', preg_replace('|/+|', '/', strtolower(preg_replace('|^'.str_replace('\\', '/', $documentRoot).'|i', '', BASE_PATH))));
-unset($documentRoot);
 
 include(BASE_PATH.'/libs/Fol/Loader.php');
 
