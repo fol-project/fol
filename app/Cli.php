ed1b035499075fa628af8352339b7839c22026ab
oscarotero <oom@oscarotero.com>
2014-03-06 00:24:49 +0100

    Melloras nos comandos

--- app/Cli.php
+++ app/Cli.php
@@ -5,24 +5,69 @@ use Fol\Terminal;
 use Fol\Http\Request;
 
 class Cli {
+
 	public static function execute (array $argv) {
 		$fnName = $argv[1];
 
+		//Execute a defined function
 		if (method_exists('App\\Cli', $fnName)) {
-			return (new static())->$fnName($argv);
+			return self::$fnName($argv);
 		}
 
-		if (in_array($fnName, ['GET', 'POST', 'HEAD', 'PUT', 'DELETE'])) {
-			list($path, $params) = Terminal::parseOptions($argv, [2 => Terminal::OPTION_REQUIRED]);
-
-			$request = Request::create(parse_url(BASE_URL, PHP_URL_PATH).$path[2], $path[1], $params);
-
-			$app = new App();
-			$app($request)->send();
-
-			die();
-		}
-
-		throw new \Exception('No valid command');
+		//or execute a request
+		self::executeRequest($argv);
 	}
+
+
+
+	/**
+	 * Execute a request from cli
+	 *
+	 * Example:
+	 * $ php fol GET /some/path?param=value
+	 */
+	public static function executeRequest (array $options) {
+		$options = Terminal::parseOptions($options, [
+			1 => ['GET', 'POST', 'HEAD', 'PUT', 'DELETE'],
+			2 => Terminal::OPTION_REQUIRED
+		]);
+
+		$method = $options[0][1];
+		$url = BASE_URL.$options[0][2];
+		$params = $options[1];
+
+		$request = Request::create($url, $method, $params);
+
+		$app = new App();
+		$app($request)->send();
+
+		die();
+	}
+
+
+
+	/**
+	 * Edit configuration values from cli
+	 *
+	 * Example:
+	 * $ php fol config database
+	 */
+	public static function config (array $options) {
+		$app = new App();
+		$name = isset($options[2]) ? $options[2] : null;
+
+		if (!$name || !($config = $app->config->get($name))) {
+			die("The config '$name' is not defined");
+		}
+
+		foreach ($config as $k => &$value) {
+			$value = Terminal::prompt("Config > {$name}.{$k} = '{$value}' > ", $value);
+		}
+
+		$app->config->set($name, $config)->saveFile($name);
+	}
+
+
+
+	// Place here your custom functions...
 }
--- constants.php
+++
@@ -1,6 +0,0 @@
-<?php
-return [
-	'ENVIRONMENT' => 'development',
-	'BASE_URL' => 'http://localhost',
-	'PUBLIC_DIR' => '/public'
-];
