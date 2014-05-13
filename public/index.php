3406fcc19c291204b524e82e08f8ad6396cefaa4
oscarotero <oom@oscarotero.com>
2014-05-13 21:38:37 +0200

    problemas de server-cli manexando urls con query

--- public/index.php
+++ public/index.php
@@ -1,8 +1,16 @@
 <?php
 
 //php -S localhost:8000 index.php
-if ((php_sapi_name() === 'cli-server') && is_file(__DIR__.$_SERVER['REQUEST_URI'])) {
-	return false;
+if (php_sapi_name() === 'cli-server') {
+	$file = __DIR__.$_SERVER['REQUEST_URI'];
+
+	if (strpos($file, '?')) {
+		$file = strstr($file, '?', true);
+	}
+
+	if (is_file($file)) {
+		return false;
+	}
 }
 
 require '../bootstrap.php';
