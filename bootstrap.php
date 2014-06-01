d18ec432b928a943c2805df9a03a6b93da07ec4c
oscarotero <oom@oscarotero.com>
2014-06-01 13:44:35 +0200

    Definir unhas constantes por defecto se se lanza fol usando o servidor de php

--- bootstrap.php
+++ bootstrap.php
@@ -1,13 +1,24 @@
 <?php
+use Fol\Http\Globals;
+
 define('ACCESS_INTERFACE', (php_sapi_name() === 'cli') ? 'cli' : 'http');
 define('BASE_PATH', str_replace('\\', '/', __DIR__));
 
-//Define basic constants
-foreach (require 'constants.php' as $name => $value) {
-    define($name, $value);
-}
-
-unset($name, $value);
 
 //Init the composer loader
 $composer = require BASE_PATH.'/vendor/autoload.php';
+
+
+//Define basic constants
+$constants = require 'constants.php';
+
+if (php_sapi_name() === 'cli-server') {
+	$constants['BASE_URL'] = Globals::getScheme().'://'.Globals::get('SERVER_NAME').':'.Globals::getPort();
+	$constants['PUBLIC_DIR'] = '';
+}
+
+foreach ($constants as $name => $value) {
+	define($name, $value);
+}
+
+unset($name, $value, $constants);
