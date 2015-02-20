d99e069bcfd6683a16896545955476df2eed1c66
oscarotero <oom@oscarotero.com>
2015-02-20 12:32:41 +0100

    engadida a configuracion de reporte de erros por defecto

--- bootstrap.php
+++ bootstrap.php
@@ -3,6 +3,10 @@ if (!ini_get('date.timezone')) {
 	ini_set('date.timezone', 'Europe/Madrid');
 }
 
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
 require __DIR__.'/vendor/autoload.php';
 
 Fol\Fol::init(__DIR__, 'env.php');
