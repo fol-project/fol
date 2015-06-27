c0c32792d6e4b81d63d55889d37b668e0085ae0a
oscarotero <oom@oscarotero.com>
2015-06-27 14:44:56 +0200

    melloras instalacion

--- install.php
+++ install.php
@@ -4,7 +4,12 @@ if (!is_file('.env')) {
 	copy('.env.example', '.env');
 }
 
+//data folder
+if (!is_dir('data')) {
+	mkdir('data', 0777);
+}
+
 //logs folder
-if (!is_dir('data/log/')) {
-	mkdir('data/log/', 0777, true);
+if (!is_dir('data/log')) {
+	mkdir('data/log', 0777);
 }
