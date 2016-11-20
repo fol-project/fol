02cba5dd11e7f35dbf1a29010984c46bf98c7dcc
Oscar Otero <oom@oscarotero.com>
2016-11-20 17:55:05 +0100

    soporte para directorios no servidor de php

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 if (php_sapi_name() === 'cli-server') {
     $path = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 
-    if ($path !== '/' && is_file(__DIR__.$path)) {
+    if ($path !== '/' && (is_file(__DIR__.$path) || is_file(rtrim(__DIR__.$path, '/').'/index.html'))) {
         return false;
     }
 }
